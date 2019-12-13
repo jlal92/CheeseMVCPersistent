bafbf926848cdae9a91caef9f1cb0e5d0dc321f6
jlal92 <54960889+jlal92@users.noreply.github.com>
2019-12-13 15:37:46 -0500

    Update CategoryController.cs

--- src/CheeseMVC/Controllers/CategoryController.cs
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -1,51 +1,47 @@
+using CheeseMVC.Data;
+using CheeseMVC.Models;
+using CheeseMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Cheeses.Data;
-using Cheeses.Models;
-using Cheeses.ViewModels;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
-namespace Cheeses.Controllers
+namespace CheeseMVC.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly CheeseDbContext context;
 
-        private readonly CheeseDBContext context;
-
-        public CategoryController(CheeseDBContext dbContext)
+        public CategoryController(CheeseDbContext dbContext)
         {
             context = dbContext;
         }
 
         public IActionResult Index()
         {
-
-            List<CheeseCategory> Categories = context.Categories.ToList();
-
-            return View(Categories);
+            var allCategories = context.Categories.ToList();
+            
+            return View(allCategories);
         }
 
         public IActionResult Add()
         {
-
             AddCategoryViewModel addCategoryViewModel = new AddCategoryViewModel();
-
             return View(addCategoryViewModel);
         }
 
         [HttpPost]
         public IActionResult Add(AddCategoryViewModel addCategoryViewModel)
         {
-
             if (ModelState.IsValid)
             {
-                CheeseCategory newCategory = addCategoryViewModel.CreateCategory();
+                CheeseCategory newCategory = new CheeseCategory
+                {
+                    Name = addCategoryViewModel.Name
+                };
 
                 context.Categories.Add(newCategory);
-
                 context.SaveChanges();
 
                 return Redirect("/Category");
@@ -53,6 +49,7 @@ namespace Cheeses.Controllers
 
             return View(addCategoryViewModel);
         }
+
 
     }
 }
